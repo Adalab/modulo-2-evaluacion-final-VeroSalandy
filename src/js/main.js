ecd16bacaeee7f619de72194db82525d481966d2
Veronica <mvsalandy@gmail.com>
2023-02-28 10:26:40 +0100

    add comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,24 @@
 'use strict';
-
 // PASOS:
-//  1: obtener datos (default-list=margaritas) y pintarlos en html
-//      * 1- Obtener los datos (fetch, lista ) : Fetch
-//      * 2- Pintarlos en el HTML : innerHtml
-// 2: buscar cocktailes y cuando usuaria pulse search (con eventlistener sobre boton search), generar datos de todos los cocktailes atraves de otro fetch interpolado. Luego pintarlo en HTML
-// 3: Guardar los favoritos de la usuaria
-// //   * 1- Crear una lista para los favoritos en el HTML
-//      * 2- Crear una lista de datos de los favoritos: list guarda objetos de resultado.
-//      * 3- Buscar con ese id en el listado (original)de ***cockteles que cocktail tiene el id del curren target, lo hacemos con un find (devuelve el primer elemento y solo un elemento ***objeto)
-//      * 4- La guardo en el listado de favoritos: push
-//      * 5- Pintar en el listado HTML de favoritos: renderFavoritos innerHTML, ¿se puede reutilizar algun función que ya tenemos?
-//      * 6 - Comprobar si ya existe (findIndex) y lo elimino de la lista de favoritos (splice)
-//      * * */
+/*1: obtener datos (default-list=margaritas) y pintarlos en html
+ * 1- Obtener los datos (fetch, lista ) : Fetch
+ * 2- Pintarlos en el HTML : innerHtml */
 
-//4: Almacenamiento local con local storage
+/*2: buscar cocktailes y cuando usuaria pulse search (con eventlistener sobre boton search), generar datos de todos los cocktailes atraves de otro fetch interpolado. Luego pintarlo en HTML */
+
+/*3: Guardar los favoritos de la usuaria
+ * 1- Crear una lista para los favoritos en el HTML
+ * 2- Crear una lista de datos de los favoritos: list guarda objetos de resultado.
+ * 3- Buscar con ese id en el listado (original)de ***cockteles que cocktail tiene el id del curren target, lo hacemos con un find (devuelve el primer elemento y solo un elemento ***objeto)
+ * 4- La guardo en el listado de favoritos: push
+ * 5- Pintar en el listado HTML de favoritos: renderFavoritos innerHTML, ¿se puede reutilizar algun función que ya tenemos?
+ * 6 - Comprobar si ya existe (findIndex) y lo elimino de la lista de favoritos (splice)*/
+
+/*4: Almacenamiento local con local storage.
+ * 1 - guardar local en local storage
+ * 2 - obtener los datos guardados en local storage */
+
+//5. Reset BTN, eliminar/borrar lista de favoritos y del localstorage, con el add event listener sobre ese boton, para que no se vuelvan a pintar
 
 //variables
 const url =
@@ -29,15 +33,16 @@ const errorMsj = document.querySelector('.js-error-message');
 let resultListData = [];
 let favoriteListData = [];
 
-//buscar en Local Storage y pintarlo en html(que es el favoritelist)
+//4.2 FUNCTION PARA OBTENER en Local Storage los datos guardados y pintarlo en html en favoritelist
 const storedCocktail = JSON.parse(localStorage.getItem('favoriteCocktail'));
 if (storedCocktail !== null) {
   // tambien lo puede escribir (storedCocktail) solo que es lo mismo
   favoriteListData = storedCocktail;
   renderFavorite(favoriteList);
 }
-//   resultList.classList.add('selected');
-//fetch para obtener (default)datos que serian (margaritas)
+
+//1. FETCH, PARA OBTENER LOS DATOS
+//1.1 FETCH CON LOS CUAL OBTENGO DATOS(DEFAULT QUE SERIA MARGARITAS)
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
@@ -45,7 +50,7 @@ fetch(url)
     renderResult(resultList);
   });
 
-//funcion Pintarlos en el HTML cockteles
+//1.2 FUNCION PARA PINTARLOS (default/input cocktails) EN HTML (en RESULTLIST se pintan los cocktails que son los cocktails por defecto al levantarse la pagina (las margaitas) o los cocktails que la usuaria introdujo en el input --> con el bucle 'for of' utilizando el findIndex verifico si idDrink del array resultlistdata es igual al idDrink del array favoritelistdata (hace comparacion). Sí el indexcocktail no esta quiro que me pinte el resultlist sin class 'selected', PERO si si esta que me pinte el resultlist con class 'selected'
 function renderResult(resultList) {
   resultList.innerHTML = '';
   for (const eachCocktail of resultListData) {
@@ -71,7 +76,7 @@ function renderResult(resultList) {
   addEventToCocktail();
 }
 
-//funcion Pintarlos en lista favorites en el HTML
+//3.5 FUNCION PARA PINTAR LISTA DE FAVORITOS EN HTML (en FAVORITELIST se pintan los cocktails que la usuaria ha seleccionado, que seran los que se pintan en lista de favoritos
 function renderFavorite(favoriteList) {
   favoriteList.innerHTML = '';
   for (const eachCocktail of favoriteListData) {
@@ -86,11 +91,12 @@ function renderFavorite(favoriteList) {
   //addEventToCocktail();
 }
 
+//FUNCION PARA PINTAR MENSAJE DE ERROR
 function renderErrorMsj(msj) {
   errorMsj.innerHTML = msj;
 }
 
-//funcion manejadora handlelickBtnSearch
+//2. FUNCION PARA buscar cocktailS y cuando usuaria pulse search (con eventlistener sobre boton search), generar datos de todos los cocktailes atraves de otro fetch interpolado, que utilizada el inputSearch.value = que es el valor que usuaria ha puesto en el input. Si el valor del input es vacio, se pintara mensaje de error. Sino se pintara la lista de resultado con valor/cocktail que usuaria puso en input. Luego se pinta lista de resultado en HTML llamando a la funcion renderresult.
 function handleClickBtnSearch(ev) {
   ev.preventDefault();
   fetch(
@@ -111,37 +117,46 @@ function handleClickBtnSearch(ev) {
     });
 }
 
-//funcion para cuando le damos click a uno de los cocktails
+/*/*3: Guardar los favoritos de la usuaria
+ * 1- Crear una lista para los favoritos en el HTML
+ * 2- Crear una lista de datos de los favoritos: list guarda objetos de resultado.
+ * 3- Buscar con ese id en el listado (original)de ***cockteles que cocktail tiene el id del curren target, lo hacemos con un find (devuelve el primer elemento y solo un elemento ***objeto)
+ * 4- La guardo en el listado de favoritos: push
+ * 5- Pintar en el listado HTML de favoritos: renderFavoritos innerHTML, ¿se puede reutilizar algun función que ya tenemos?
+ * 6 - Comprobar si ya existe (findIndex) y lo elimino de la lista de favoritos (splice)*/
+
+//3. FUNCTION PARA CUANDO LE DAMOS CLICK A UNO DE LOS COCKTAILS, PARA QUE SE GUARDEN Y PINTEN EN EL LISTADO DE FAVORITOS. UTILIZANDO EL 'FIND Y FINDINDEX'.
 function handleClickOfEachCocktail(ev) {
   const idSelectedCocktail = ev.currentTarget.id;
-  // find; nos devuelve primer elemento que cumpla condicion, aqui es el ID
+  // 3.3 FIND; nos devuelve primer elemento del array que cumpla condicion, aqui es el ID
   const selectedCocktail = resultListData.find(
     (cocktailItem) => cocktailItem.idDrink === idSelectedCocktail
   );
 
-  //comprobar si ya existe el favorito con findIndex y nos devuelve posicion donde esta el elemento o -1 cuando no esta
+  // FINDINDEX: comprobar si ya existe el favorito con findIndex y nos devuelve posicion donde esta el elemento o si nos devuelve -1 es que no esta
   const indexCocktail = favoriteListData.findIndex(
     (cocktailItem) => cocktailItem.idDrink === idSelectedCocktail
   );
 
   if (indexCocktail === -1) {
-    //-1 significa que no esta en lista favo
+    //-1 significa que no esta en lista favoritos
 
-    //guardar ese objeto (que obtuvimos con find) en listado de favoritos(FLData): con push
+    //3.2 y 3.4 guardar ese objeto (que obtuvimos con find) en listado (array) de favoritos(FLData): con push --> el array lo cree arriba como let favoriteListData = [];
     favoriteListData.push(selectedCocktail);
     ev.currentTarget.classList.add('selected');
   } else {
-    // si SI esta en el listado de favoritos, con splice lo puedes eliminar
+    //  3.6. si SI esta en el listado de favoritos, con splice lo  elimino -->
     favoriteListData.splice(indexCocktail, 1);
     ev.currentTarget.classList.remove('selected');
   }
-  //pintar en el listado de favorits en html
+  //3.5 pintar en el listado de favorits en html con la funcion de renderFavorite
   renderFavorite(favoriteList);
 
+  //4.1  GUARDAR (datos) local en LOCALSTORAGE
   localStorage.setItem('favoriteCocktail', JSON.stringify(favoriteListData));
 }
 
-//function para cada uno de los li/cocktailes para manejar el addevent listener
+//function para manejar el addevent listener de cada uno de los li/cocktailes. Ya que con el bucle va recorriendo cada uno de los li, no le puedo poner a cada li manual el evento add eventlistener, con esta funcion se le pone el evento de add event listener a cada li atraves del bucle a los que le dan click
 function addEventToCocktail() {
   const liElementsList = document.querySelectorAll('.js-art-li-cocktail');
 
@@ -150,6 +165,7 @@ function addEventToCocktail() {
   }
 }
 
+//5. FUNCTION PARA ELIMINAR/RESETEAR con Reset BTN, lista de favoritos y del localstorage, con el add event listener sobre ese boton, para que no se vuelvan a pintar
 function handleClickBtnReset(ev) {
   ev.preventDefault();
 
